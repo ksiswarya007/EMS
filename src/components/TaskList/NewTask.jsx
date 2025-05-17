cb6c19b98957a80e69cf8a9f7ae9b841c4a31077
ksiswarya007 <136336216+ksiswarya007@users.noreply.github.com>
2025-05-17 11:39:07 +0530

    Update NewTask.jsx

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const NewTask = ({data}) => {
+const NewTask = ({ data, taskIndex, updateTaskStatus }) => {
   return (
-   <div className='flex-shrink-0 h-full w-[300px] p-5  bg-blue-400 rounded-xl'>
-        <div className='flex justify-between items-center'>
-            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
-            <h4 className='text-sm'>{data.date}</h4>
-        </div>
-        <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
-        <p className='text-sm mt-2'>{data.description}</p>
-        <div className='mt-4'>
-                <button>Accept Task</button>
-        </div>
+    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
+      <div className='flex justify-between items-center'>
+        <h3 className='bg-blue-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+        <h4 className='text-sm'>{data.date}</h4>
       </div>
-  )
-}
+      <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
+      <p className='text-sm mt-2'>{data.description}</p>
+      <div className='flex justify-between mt-4'>
+        <button
+          className='bg-green-500 py-1 px-2 text-sm'
+          onClick={() => updateTaskStatus(taskIndex, 'active')}
+        >
+          Accept Task
+        </button>
+        <button
+          className='bg-red-500 py-1 px-2 text-sm'
+          onClick={() => updateTaskStatus(taskIndex, 'failed')}
+        >
+          Mark as Failed
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default NewTask
+export default NewTask;
