bd4359e9a5aa12f37a7eae81e33586e1d9a3bc88
ksiswarya007 <136336216+ksiswarya007@users.noreply.github.com>
2025-05-17 11:39:25 +0530

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,22 +4,23 @@ import NewTask from './NewTask';
 import CompleteTask from './CompleteTask';
 import FailedTask from './FailedTask';
 
-const TaskList = ({data}) => {
+const TaskList = ({ data, updateTaskStatus }) => {
   return (
     <div id='tasklist' className='h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full overflow-x-auto py-5 rounded-xl mt-10'>
-      {data.tasks.map((elem,idx)=>{
-        if(elem.active){
-          return <AcceptTask key={idx} data={elem}/> 
+      {data.tasks.map((elem, idx) => {
+        if (elem.active) {
+          return <AcceptTask key={idx} data={elem} taskIndex={idx} updateTaskStatus={updateTaskStatus} />;
         }
-        if(elem.newTask){
-          return <NewTask key={idx} data={elem}/> 
+        if (elem.newTask) {
+          return <NewTask key={idx} data={elem} taskIndex={idx} updateTaskStatus={updateTaskStatus} />;
         }
-        if(elem.completed){
-          return <CompleteTask key={idx} data={elem}/> 
+        if (elem.completed) {
+          return <CompleteTask key={idx} data={elem} />;
         }
-        if(elem.failed){
-          return <FailedTask key={idx} data={elem}/> 
+        if (elem.failed) {
+          return <FailedTask key={idx} data={elem} />;
         }
+        return null;
       })}
     </div>
   );
